4c8007453febb009df76ba89026706fcf69b8215
umarkhayyam-dev <Umarkhayyamim21@gmail.com>
2025-05-21 16:39:48 +0530

    Added Task 1 and Task 2 scripts
    Uploaded Python scripts for Task 1 (operations) and Task 2 (concatenation).

---
+++ concatination.py
@@ -0,0 +1,6 @@
+a=input('Enter the first name: ')
+b=input('Enter the second name: ')
+c=a+" "+b
+c=str(c)
+print('Hello,',c,'!', 'Welcome to python Programing')
+
---
+++ operation.py
@@ -0,0 +1,24 @@
+a=input("Enter the First number:")
+a=int(a)
+b=input("Enter the second number: ")
+b=int(b)
+'''ADDITION'''
+c=a+b
+c=int(c)
+print('ADDITION: ',c)
+
+'''SUBSTRACTION'''
+d=a-b
+d=int(d)
+print('SUBSTRACTION: ',d)
+
+'''MULTIPLICATION'''
+e=a*b
+e=int(e)
+print('MULTIPLICATION: ',e)
+
+'''DIVISION'''
+f=a/b
+f=float(f)
+print('DIVISTION: ',f)
+
